09d632440f93fec5812df6a03fc006ed485c9801
Harsha Phaneendra <harshap812@gmail.com>
2022-01-11 00:08:25 +0530

    added rqt to script file

--- src/scripts/ogm.sh
+++ src/scripts/ogm.sh
@@ -6,4 +6,6 @@ xterm -e "source devel/local_setup.bash; rosrun ogm_node ogm_node" &
 sleep 5
 xterm -e "source devel/local_setup.bash; rosrun visible_markers visible_markers" &
 sleep 5
-xterm -e "source devel/local_setup.bash; rviz rviz"
+xterm -e "source devel/local_setup.bash; rviz rviz" &
+sleep 5
+xterm -e "source devel/local_setup.bash; rqt"
