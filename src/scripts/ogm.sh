e5ffcec5d78ca30a7453ec088273238418517fee
Harsha Phaneendra <harshap812@gmail.com>
2022-01-10 18:57:37 +0530

    adding marker node

--- src/scripts/ogm.sh
+++ src/scripts/ogm.sh
@@ -4,4 +4,6 @@ xterm -e "source devel/local_setup.bash; roscore" &
 sleep 5
 xterm -e "source devel/local_setup.bash; rosrun ogm_node ogm_node" &
 sleep 5
-xterm -e "source devel/local_setup.bash; rqt --clear-config"
+xterm -e "source devel/local_setup.bash; rosrun visible_markers visible_markers" &
+sleep 5
+xterm -e "source devel/local_setup.bash; rviz rviz"
